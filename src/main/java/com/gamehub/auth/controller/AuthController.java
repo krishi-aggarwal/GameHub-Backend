6c527c0bb9bf5e13c5caccc7fd288df0aab7e7a0
krishi-aggarwal <krishi.2004.aggarwal@gmail.com>
2026-08-18 15:42:57 +0530

    feat(auth): implement user registration

---
+++ src/main/java/com/gamehub/auth/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.gamehub.auth.controller;
+
+import com.gamehub.auth.dto.RegisterRequest;
+import com.gamehub.auth.dto.RegisterResponse;
+import com.gamehub.auth.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService){
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> register(
+            @Valid @RequestBody RegisterRequest request
+            ){
+        RegisterResponse response = authService.registerUser(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+}
